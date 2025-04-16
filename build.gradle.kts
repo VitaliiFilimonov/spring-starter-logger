af73c049ff2e7b82a282706ef7d9b162ac368bb4
Vitalii Filimonov <vitalifilimonov@yandex.ru>
2025-04-16 11:50:40 +0400

    editing after reviewing

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,15 @@ plugins {
 group = "ru.homework"
 version = "1.0-SNAPSHOT"
 
+val aspectj = "1.9.20.1"
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.aspectj:aspectjweaver:1.9.20.1")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.aspectj:aspectjweaver:$aspectj")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/ru/homework/aspect/TaskAspect.java
+++ src/main/java/ru/homework/aspect/TaskAspect.java
@@ -10,11 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import ru.homework.properties.LoggerProperties;
 
 @Aspect
-@Component
 public class TaskAspect {
 
     private final Logger logger = LoggerFactory.getLogger(TaskAspect.class);
